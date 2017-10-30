d49f0bb51068443d7491c85703dac4cfd05a593a
Flippye <clara.muller@epfl.ch>
2017-10-30 19:05:36 +0100

    adding doxygen documentation

--- cortex.hpp
+++ cortex.hpp
@@ -8,37 +8,83 @@
 class cortex : public neuron {
 	private:
 	
-	std::vector<neuron*> neurons_; 	//neuron network
-	std::vector<std::vector<int>> connexionMatrix_; 	//matrix that represents the connexion between the neurons (lines send a spike, columns receive it)
+	std::vector<neuron*> neurons_; 	///<neuron network
+	std::vector<std::vector<int>> connexionMatrix_; 	///<matrix that represents the connexion between the neurons (lines send a spike, columns receive it)
 	
-	unsigned int neuronNb_; 		//number of neurons inside neurons_
-	const double delay_ = 0.9;		//synaptic delay [ms]
+	unsigned int neuronNb_; 		///<number of neurons inside neurons_
+	const double delay_ = 0.9;		///<synaptic delay [ms]
 	
-	double h_ = 0.1; 							//integration stepsize TEMPORARY SOLUTION
-	const int delaySteps_ = (int) (delay_/h_); 	//number of time steps of the synaptic delay (int conversion)
+	double h_ = 0.1; 							///<integration stepsize TEMPORARY SOLUTION
+	const int delaySteps_ = (int) (delay_/h_); 	///<number of time steps of the synaptic delay (int conversion)
 	
 	
 	
 	public:
-	cortex(unsigned int neuronNb = 0);	//constructor for excitatory neurons (per default)
-	cortex(unsigned int nbNeuronsE, unsigned int nbNeuronsI); 	//constructor allowing to make a distinction between excitatory and inhibitory neurons
-	~cortex();							//destructor
+	/**
+	@brief : constructor per default, construct only excitatory neurons
+	@param neuronNb : number of neurons to create the cortex with
+	*/
+	cortex(unsigned int neuronNb = 0);
 	
 	/**
-	 * at param
-	 * at return
-	 * */
+	@brief : constructor allowing to make a distinction between excitatory and inhibitory neurons
+	@param nbNeuronsE : number of excitatory neurons
+	@param nbNeuronsI : number of inhibitory neurons
+	*/
+	cortex(unsigned int nbNeuronsE, unsigned int nbNeuronsI);
+	
+	/**
+	@brief : destructor of the class
+	*/
+	~cortex();
+	
+	/**
+	@brief : getter for the integration stepsize
+	@return : integration stepsize
+	*/
 	double getH() const;
-	 
-	void increaseNeuronNumber();		//increase the instance that keeps in mind the number of neurons inside the vector neurons_
-	void pushbackToNeurons_(neuron* const neuronToAdd);			//adds a pointer to neuron inside the vector neurons_
-	void receiveSpike(neuron receiver, bool whichSign);			//puts a spike in the ring buffer of a neuron receiver
 	
-	void addAConnexion(unsigned int row, unsigned int column);	//add a connexion between two neurons in the connexion matrix
-	void createRandomConnexionMatrix(unsigned int nbNeuronsE, unsigned int nbNeuronsI);	//create a randon connexion matrix with the same number of connexions between inhibatory and excitatory neurons
+	/**
+	@brief : function that keeps track of the number of neurons inside the vector containing all the neurons (called neurons_)
+	*/
+	void increaseNeuronNumber();
 	
-	void updateAll(unsigned int nit);	//update all neurons inside the vector neurons_
+	/**
+	@brief : adds a pointer to neuron inside the vector neurons_
+	@param neuronToAdd : pointer to neuron that we want to add to our vector containing all the pointers to neurons
+	*/
+	void pushbackToNeurons_(neuron* const neuronToAdd);
 	
+	/**
+	@brief : puts a spike in the ring buffer of a neuron receiver, taking into account the state of the sender neuron (excitatory or inhibitory)
+	@param receiver : neuron that will receive a spike to store inside its ring buffer
+	@param whichSign : state of the neuron sending the spike (excitatory or inhibitory)
+	*/
+	void receiveSpike(neuron receiver, bool whichSign);
+	
+	/**
+	@brief : adds a connexion between two neurons in the connexion matrix
+	@param row : index of the row to store the information of the connexion
+	@param column : index of the column to store the information of the connexion
+	*/
+	void addAConnexion(unsigned int row, unsigned int column);
+	
+	/**
+	@brief : creates a randon connexion matrix with the same number of connexions between inhibatory and excitatory neurons
+	@param nbNeuronsE : number of excitatory neurons
+	@param nbNeuronsI : number of inhibitory neurons
+	*/
+	void createRandomConnexionMatrix(unsigned int nbNeuronsE, unsigned int nbNeuronsI);
+	
+	/**
+	@brief : updates all neurons inside the vector neurons_
+	@param nit : number of iteration of the simulation
+	*/
+	void updateAll(unsigned int nit);
+	
+	/**
+	@brief : creates a document called "TimeFlies.csv" storing the time of all the spikes and the number of the neuron spiking
+	*/
 	void timeOfTheSpikes();
 };
 
